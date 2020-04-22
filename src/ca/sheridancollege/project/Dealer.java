60083458f1d986a622738ba7b48df03fb4d72fb2
BASSAM SAMMAK <61026051+BassamSammak@users.noreply.github.com>
2020-04-22 18:36:03 -0400

    Update Dealer.java

--- src/ca/sheridancollege/project/Dealer.java
+++ src/ca/sheridancollege/project/Dealer.java
@@ -11,41 +11,48 @@ import java.util.ArrayList;
  *
  * @author Cloud
  */
-public class Dealer extends Player{
-    
-    ArrayList<BlackjackCard> dealerHand = new ArrayList<>();
+public class Dealer extends Player {
+
+    ArrayList<BlackJackCard> dealerHand = new ArrayList<>();
     int dealerValue = 0;
     int aceAmount = 0;
     Card[] dealerHandA;
-    
-    
-    Dealer(){
+
+    Dealer() {
         super("Dealer");
-   
+
     }
-    
-    public BlackjackCard addCard(Deck deck){
-        
+
+    public BlackJackCard addCard(Deck deck) {
+
         dealerHand.add(deck.drawCard());
-        dealerHand.add(deck.drawCard());        
+        dealerHand.add(deck.drawCard());
         dealerValue = dealerHand.get(0).getValue() + dealerHand.get(1).getValue();
         return dealerHand.get(1);
-        
-        
+
     }
-    
-    public BlackjackCard dealerHandValue(Deck deck){
-        if(dealerValue < 17){
+
+    public void addHand(Deck deck) {
+
+        dealerHand.add(deck.drawCard());
+
+    }
+
+    public BlackJackCard dealerHandValue(Deck deck) {
+        if (dealerValue < 17) {
             dealerHand.add(deck.drawCard());
-            dealerValue += dealerHand.get(dealerHand.size()-1).getValue();
+        } else if (dealerValue >= 17) {
+            dealerValue = 0;
+            for (int i = 0; i < dealerHand.size(); i++) {
+                dealerValue += dealerHand.get(i).getValue();
+            }
         }
-        return dealerHand.get(dealerHand.size()-1);
+        return dealerHand.get(dealerHand.size() - 1);
     }
-    
+
     @Override
     public void play() {
-        
-    }
-    
-}
 
+    }
+
+}
