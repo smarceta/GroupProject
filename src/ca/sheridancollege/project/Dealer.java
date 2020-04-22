eeeb4f2ce8ea2157be0c552b856e80f073e10616
BASSAM SAMMAK <61026051+BassamSammak@users.noreply.github.com>
2020-04-21 22:21:16 -0400

    Update Dealer.java

--- src/ca/sheridancollege/project/Dealer.java
+++ src/ca/sheridancollege/project/Dealer.java
@@ -5,21 +5,33 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author Cloud
  */
 public class Dealer extends Player{
     
-    Dealer(){
-        
-        super("Dealer");
-        
+    Dealer(){        
+        super("Dealer");        
+    }
+    
+    public void draw(){
+        Deck deck = new Deck(52);
+        deck.fillDeck();
+        deck.shuffle();
+        //ArrayList list = deck.deck;
+        //Random random = new Random();
+        //int randomNum = random.nextInt((52 - 1) + 1) + 1;
+        System.out.println(deck.deck.get(0));
+        //System.out.println(list.indexOf(randomNum));
     }
     
     @Override
     public void play() {
-        
+        System.out.println("yo");
     }
     
 }
