93f47bd6e56d18d22cd0a342563e00fdf172c0bb
BASSAM SAMMAK <61026051+BassamSammak@users.noreply.github.com>
2020-04-22 23:43:31 -0400

    Update BlackjackGame.java

--- src/ca/sheridancollege/project/BlackjackGame.java
+++ src/ca/sheridancollege/project/BlackjackGame.java
@@ -1,8 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+-This class is used to determine the winner by calculating the values
+*/
 package ca.sheridancollege.project;
 
 /**
