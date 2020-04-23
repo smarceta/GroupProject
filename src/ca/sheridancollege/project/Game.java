ddb30f975f1e67292916f8597f25c459d3499b3f
BASSAM SAMMAK <61026051+BassamSammak@users.noreply.github.com>
2020-04-22 22:31:07 -0400

    Update Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -57,7 +57,7 @@ public abstract class Game
      * When the game is over, use this method to declare and display a winning
      * player.
      */
-    public abstract void declareWinner();
+    public abstract void declareWinner(Dealer dealer, HumanPlayer player);
 
    
     
